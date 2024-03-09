38d5d3271a54434aa96537f310461eda754ecb79
Hrikdipta Kundu <hrikdiptakundu123@gmail.com>
2024-03-09 11:17:47 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 const app =express();
 const port =3000;
+
+dotenv.config();
+console.log(process.env.MONGODBURL)
+mongoose.connect(process.env.MONGODBURL).then(()=>console.log("mongodb connected !!")).catch((err)=>{  throw err})
+
 
 
 app.listen(port,()=>{
