31cfb9cbe65623221e871657e2e2c41e381ea39d
Hrikdipta Kundu <hrikdiptakundu123@gmail.com>
2024-03-10 12:30:33 +0530

    create and add the footer and logo component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
-import {BrowserRouter, Route,Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import SignIn from './pages/Sign-in'
 import SignUp from './pages/Sign-up'
 import Header from './components/Header'
+import FooterCom from './components/Footer'
 function App() {
   return (
     <>
-    <h1 className='text-lg'></h1>
-    
-    <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About />} />
-      <Route path='/sign-up' element={<SignUp/>}/>
-      <Route path='/sign-in' element={<SignIn/>}/>
-    </Routes>
-    </BrowserRouter>
+      <h1 className='text-lg'></h1>
+
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/sign-in' element={<SignIn />} />
+        </Routes>
+        <FooterCom/>
+      </BrowserRouter>
     </>
-    
+
   )
 }
 
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Logo from './Logo';
+import { Link } from 'react-router-dom';
+import { Footer } from 'flowbite-react'
+import { BsFacebook, BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs';
+function FooterCom() {
+  return (
+    <Footer container>
+      <div className="w-full">
+        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+          <div>
+            <Logo />
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="Company" />
+              <Footer.LinkGroup col>
+                <Link  to='/about' ><Footer.Link href="#" as='div' >About us</Footer.Link></Link>
+                <Link to="#"><Footer.Link href="#" as='div'>Pricing</Footer.Link></Link>
+                <Link to="#"><Footer.Link href="#" as='div'>Contact us</Footer.Link></Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="https://github.com/hrikdipta">Github</Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright by="Hrikdipta Kundu" year={2024} />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="https://www.facebook.com/profile.php?id=100072202299973" target='_balnk' icon={BsFacebook} />
+            <Footer.Icon href="https://www.instagram.com/hrikdiptakundu/" target='_balnk' icon={BsInstagram} />
+            <Footer.Icon href="#" target='_balnk' icon={BsTwitter} />
+            <Footer.Icon href="https://github.com/hrikdipta" target='_balnk' icon={BsGithub} />
+            
+          </div>
+        </div>
+      </div>
+    </Footer>
+  )
+}
+
+export default FooterCom
