3110527bfd961d018f0873a3b80ef5d6c1ed5ba8
Hrikdipta Kundu <hrikdiptakundu123@gmail.com>
2024-03-16 18:22:07 +0530

    update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,38 @@
+import { errorHandler } from "../utils/error.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 const test=(req,res)=>{
     res.send("user route")
 }
+export const updateUser=async(req,res,next)=>{
+   if(req.user.id!==req.params.userId){
+    return next(errorHandler(401,'UnAuthorized'))
+   }
+   if(req.body.password){
+        if(req.body.password.length<6){
+            return next(errorHandler(400,'Password must be atleast 6 characters'));
+        }
+        req.body.password=bcrypt.hashSync(req.body.password,10);
+   }
+   try {
+        const updatedUser =await User.findByIdAndUpdate(req.params.userId,{
+            $set:{
+                username:req.body.username,
+                email:req.body.email,
+                password:req.body.password,
+                photoURL:req.body.photoURL
+            }
+        },{new:true})
+        if(!updatedUser){
+            return next(errorHandler(404,"User not found"))
+        }
+        const {password,...rest}=updatedUser._doc;
+        res.status(200).json(rest);
+   } catch (error) {
+    next(error);
+   }
+}
+
+
 export default test; 
--- api/index.js
+++ api/index.js
@@ -4,12 +4,13 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 const app =express();
 const port =3000;
 
 app.use(cors());
-
 app.use(express.json());
+app.use(cookieParser());
 
 dotenv.config();
 mongoose.connect(process.env.MONGODBURL).then(()=>console.log("mongodb connected !!")).catch((err)=>{  throw err})
