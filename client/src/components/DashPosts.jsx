41de59ac6fa0a03d176eb04165724ea8cbb3c9fc
Hrikdipta Kundu <hrikdiptakundu123@gmail.com>
2024-03-21 01:43:53 +0530

    show user posts inside dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
-
+import React, { useEffect,useState } from 'react'
+import {useSelector} from 'react-redux'
+import { Table } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 function DashPosts() {
+  const currentUser = useSelector(state => state.user.currentUser);
+  const[posts,setPosts]=useState([]);
+  console.log(posts);
+  useEffect(()=>{
+    const fetchPosts=async()=>{
+      const res=await fetch(`/api/post/getposts?userId=${currentUser._id}`)
+      const data = await res.json();
+      if(res.ok){
+        setPosts(data.posts)
+      }
+    }
+    if(currentUser.isAdmin){
+      fetchPosts()
+    }
+  },[currentUser._id])
   return (
-    <div>
-      posts dashboard
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+      {
+        (currentUser.isAdmin && posts.length===0) ?<div className='text-md'>You don't have any post to show here</div>:
+          <Table hoverable className='shadow-md'>
+            <Table.Head className='w-full'>
+              <Table.HeadCell>Date Updated</Table.HeadCell>
+              <Table.HeadCell>Post Image</Table.HeadCell>
+              <Table.HeadCell>Post Title</Table.HeadCell>
+              <Table.HeadCell>Catagory</Table.HeadCell>
+              <Table.HeadCell><span className="sr-only">Edit</span></Table.HeadCell>
+              <Table.HeadCell><span className="sr-only">Delete</span></Table.HeadCell>
+            </Table.Head>
+
+            <Table.Body className="divide-y">
+              {
+                posts.map(post => (
+                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                      {post.updatedAt.substring(0, 10).split('-').reverse().join('-')}
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Link to={`/post/${post.slug}`}>
+                        <img className='max-w-40  object-cover bg-slate-500' src={post.image} alt={post.title}/>
+                      </Link>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Link to={`/post/${post.slug}`}>
+                        {post.title}
+                      </Link>
+                    </Table.Cell>
+                    <Table.Cell>{post.catagory}</Table.Cell>
+                    <Table.Cell>
+                      <Link to={`/update-post${post._id}`} className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                        Edit
+                      </Link>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <span className="font-medium text-red-600 hover:underline dark:text-red-500 cursor-pointer">
+                        Delete
+                      </span>
+                    </Table.Cell>
+                  </Table.Row>
+                ))
+              }
+            </Table.Body>
+          </Table>
+      }
+      
     </div>
   )
 }
