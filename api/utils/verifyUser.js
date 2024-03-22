4257b9c7d8628ae083ca72dc465f71088e71cf47
Hrikdipta Kundu <hrikdiptakundu123@gmail.com>
2024-03-23 01:27:46 +0530

    add show more funtionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -3,7 +3,7 @@ import { errorHandler } from "./error.js";
 export const verifyUser=(req,res,next)=>{
     const token = req.cookies.token;
     if(!token){
-        return next(errorHandler(401,'UnAuthorized'));
+        return next(errorHandler(401,'no token'));
     }
     jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
         if(err){
--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -5,19 +5,39 @@ import { Link } from 'react-router-dom';
 function DashPosts() {
   const currentUser = useSelector(state => state.user.currentUser);
   const[posts,setPosts]=useState([]);
-  console.log(posts);
+  const[showMore,setShowMore]=useState(true);
+  
   useEffect(()=>{
     const fetchPosts=async()=>{
       const res=await fetch(`/api/post/getposts?userId=${currentUser._id}`)
       const data = await res.json();
       if(res.ok){
         setPosts(data.posts)
+        if(data.totalPosts<=9){
+          setShowMore(false);
+        }
       }
     }
     if(currentUser.isAdmin){
       fetchPosts()
     }
   },[currentUser._id])
+
+  const handleShowMore=async()=>{
+    const startIndex=posts.length;
+    try {
+      const res=await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data=await res.json();
+      if(res.ok){
+        setPosts([...posts,...data.posts])
+        if(data.posts.length<=9){
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {
@@ -66,7 +86,9 @@ function DashPosts() {
             </Table.Body>
           </Table>
       }
-      
+      {
+        showMore && <button onClick={handleShowMore} className='text-teal-500 px-4 py-4 self-center w-full'>Show more</button>
+      }
     </div>
   )
 }
